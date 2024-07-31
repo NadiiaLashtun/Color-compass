9638e502aa86330c4c385c4bc5f497f95c606d2d
FlorSmile <flor.smile1@i.ua>
2024-07-31 23:52:05 +0300

    Fix scroll buttons position

--- src/components/ScrollButton.jsx
+++ src/components/ScrollButton.jsx
@@ -23,15 +23,15 @@ function ScrollButton() {
   };
 
   return (
-    <div className='flex justify-center space-x-16 cursor-pointer'>
+    <div className='relative flex justify-center space-x-16 cursor-pointer'>
       <button
         onClick={goBack}
-        className={`absolute bottom-[18%] lg:bottom-[23%] xl:bottom-[22%] w-4 h-4 bg-primary-white border border-brand-green border-opacity-40 rounded-full shadow-shape transition-all duration-300 hover:bg-primary-color hover:border-brand-red hover:border-opacity-40'
+        className={`absolute bottom-9 w-4 h-4 bg-primary-white border border-brand-green border-opacity-40 rounded-full shadow-shape transition-all duration-300 hover:bg-primary-color hover:border-brand-red hover:border-opacity-40'
           ${currentPageIndex <= 0 ? "opacity-50 cursor-not-allowed" : ""}`}
       ></button>
       <button
         onClick={goForward}
-        className={`absolute bottom-[18%] lg:bottom-[23%] xl:bottom-[22%] w-4 h-4 bg-primary-white border border-brand-green border-opacity-40 rounded-full shadow-shape transition-all duration-300 hover:bg-primary-color hover:border-brand-red hover:border-opacity-40 
+        className={`absolute bottom-9 w-4 h-4 bg-primary-white border border-brand-green border-opacity-40 rounded-full shadow-shape transition-all duration-300 hover:bg-primary-color hover:border-brand-red hover:border-opacity-40 
           ${
             currentPageIndex >= routes.length - 2
               ? "opacity-50 cursor-not-allowed"
